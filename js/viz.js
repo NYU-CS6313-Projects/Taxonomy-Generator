de85c528f3ae79f9df0df68a0bcf09398592b55b
wei zhang <weizhangtju1016@gmail.com>
2015-04-24 11:44:01 -0400

    Update viz.js

--- js/viz.js
+++ js/viz.js
@@ -1,30 +1,64 @@
-var viz = angular.module('viz', ['vizServices','ngSanitize']);
+var viz = angular.module('viz', ['vizServices','ngSanitize','common.dragdrop']);
 
-viz.controller('vizCtrl', function ($scope, db) {
+viz.controller('vizCtrl', function ($scope, db, DragDropHandler) {
+
     $scope.topic = "climate change";
+
     $scope.taxonomy = {
         any: ["climate change"],
         and: [],
         not: [],
-        ignore: ["change"]
-    }
-    
+        ignore: ["change"],
+        turnoff:[]
+    };
+
+    $scope.objects = [
+        {
+            id: 1,
+            name: 'New Item 1'
+        },
+        {
+            id: 2,
+            name: 'New Item 2'
+        }
+    ];
+
+
     $scope.keywords = [];
     
     //Taxonomy operators
     $scope.addKeyword = function(list){
         var word = prompt("Type keyword");
         list.push(word);
-        $scope.load();
-        
+        // $scope.load();
     }
-    
-    $scope.removeKeyword = function(word, list){
-        list.remove(word);
+
+    $scope.removeKeyword = function(word,List) { 
+      for (var list in $scope.taxonomy) {
+        if ($scope.taxonomy[list]==List) {
+          $scope.taxonomy[list] = _.reject($scope.taxonomy[list], function(w) {
+            return w == word; 
+          });
+        }
+      }
+    };
+
+    //drag-drop-sort
+    $scope.moveObject = function(from, to, fromList, toList) {
+        var item = $scope.taxonomy[fromList][from];
+        DragDropHandler.addObject(item, $scope.taxonomy[toList], to);
+        $scope.taxonomy[fromList].splice(from, 1);
     }
+
+    $scope.createObject = function(object, to, list) {
+        var newItem = angular.copy(object);
+        newItem.id = Math.ceil(Math.random() * 1000);
+        DragDropHandler.addObject(newItem, $scope.taxonomy[list], to);
+    };
     
-    //Data operators
-    $scope.load = function(){
+  
+    // Data operators
+     $scope.load = function(){
         db.load($scope.taxonomy.any, $scope.taxonomy.not, $scope.taxonomy.ignore).then(function(r){
             $scope.keywords = r.aggregations.NAME.buckets;
             $scope.tweets = r.hits.hits;
